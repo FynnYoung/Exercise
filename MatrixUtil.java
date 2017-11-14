65a5468598e558db3a840376459e8e49be9bed08
FynnYoung <33476001+FynnYoung@users.noreply.github.com>
2017-11-14 23:27:04 +0800

    Add files via upload

---
+++ MatrixUtil.java
@@ -0,0 +1,126 @@
+//package machine;
+
+public class MatrixUtil {
+	public double determinant(double[][] matrix){ // ÐÐÁÐÊ½
+		int dim = matrix.length;
+		double det = 0;
+		if(dim==2){
+			det = matrix[0][0]*matrix[1][1]-matrix[0][1]*matrix[1][0];
+		}else{
+			for(int i=0; i<dim; i++){
+				double sign = Math.pow(-1, i);
+				double[][] sub = subMatrix(matrix, 0, i);
+				det += matrix[0][i]*sign*determinant(sub);
+			}
+		}
+		return det;
+	}
+	public double[][] subMatrix(double[][] matrix, int i, int j){
+		int dim = matrix.length;
+		double[][] sub = new double[dim-1][dim-1];
+		for(int ii=0; ii<i; ii++){
+			for(int jj=0; jj<j; jj++){
+				sub[ii][jj] = matrix[ii][jj];
+			}
+			for(int jj=j; jj<dim-1; jj++){
+				sub[ii][jj] = matrix[ii][jj+1];
+			}
+		}
+		for(int ii=i; ii<dim-1; ii++){
+			for(int jj=0; jj<j; jj++){
+				sub[ii][jj] = matrix[ii+1][jj];
+			}
+			for(int jj=j; jj<dim-1; jj++){
+				sub[ii][jj] = matrix[ii+1][jj+1];
+			}
+		}
+		return sub;
+	}
+	
+	public double[][] minor(double[][] matrix){ // Óà×Ó¾ØÕó
+		int dim = matrix.length;
+		double[][] minor = new double[dim][dim];
+		for(int i=0; i<dim; i++){
+			for(int j=0; j<dim; j++){
+				minor[i][j] = Math.pow(-1, i+j)*determinant(subMatrix(matrix, i, j));
+			}
+		}
+		return minor;
+	}
+	
+	public double[][] adjugate(double[][] matrix){ // °éËæ¾ØÕó
+		return transpose(minor(matrix));
+	}
+	
+	public double[][] transpose(double[][] matrix){ // ×ªÖÃ
+		int dim = matrix.length;
+		double[][] transpose = new double[dim][dim];
+		for(int i=0; i<dim; i++){
+			for(int j=0; j<dim; j++){
+				transpose[i][j] = matrix[j][i];
+			}
+		}
+		return transpose;
+	}
+	
+	public double[][] inverse(double[][] matrix){ // Äæ¾ØÕó
+		double det = determinant(matrix);
+		double[][] adj = adjugate(matrix);
+		int dim = matrix.length;
+		double[][] inverse = new double[dim][dim];
+		for(int i=0; i<dim; i++){
+			for(int j=0; j<dim; j++){
+				inverse[i][j] = adj[i][j]/det;
+			}
+		}
+		
+		return inverse;
+	}
+	
+	public double[][] multiplication(double[][] a, double[][] b){
+		if(a[0].length!=b.length){
+			System.out.println("wrong dimensions!");
+			return null;
+		}
+		double[][] result = new double[a.length][b[0].length];
+		for(int i=0; i<result.length; i++){
+			for(int j=0; j<result[0].length; j++){
+				double sum = 0;
+				for(int k=0; k<a[0].length; k++){
+					sum += a[i][k]*b[k][j];
+				}
+				result[i][j] = sum;
+			}
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//double[][] a = {{2,1,-1},{4,-1,1},{201,102,-99}}; // -18
+		//double[][] a = {{1,1,-1,3},{-1,-1,2,1},{2,5,2,4},{1,2,3,2}}; // 33
+		//double[][] a = {{1,0,-1,2},{-2,1,3,1},{0,1,0,-1},{1,3,4,-2}}; // 31
+		//double[][] matrix = {{4,3,0,0},{1,4,3,0},{0,1,4,3},{0,0,1,4}};
+		MatrixUtil m = new MatrixUtil();
+		double[][] a = {{1,2},{3,4}};
+		double[][] b = {{2},{1}};
+		double[][] r = m.multiplication(a, b);
+		//double[][] inverse = m.inverse(matrix);
+		for(int i=0; i<r.length; i++){
+			for(double j : r[i]){
+				System.out.print(j+"\t");
+			}
+			System.out.println();
+		}
+		//System.out.println(m.determinant(a));
+		//double[][] sub = m.subMatrix(a, 2, 1);
+		//for(int i=0; i<sub.length; i++){
+			//for(double j : sub[i]){
+				//System.out.print(j+" ");
+			//}
+			//System.out.println();
+		//}
+
+	}
+
+}
